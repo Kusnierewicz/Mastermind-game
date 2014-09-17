8906ce97d92ece4c083bb9a84086e55c98faafb4
Kusnierewicz <kusnierewicz@gmail.com>
2014-09-17 07:02:12 +0200

    after few fixes

--- lib/mastermind/ai.rb
+++ lib/mastermind/ai.rb
@@ -41,5 +41,34 @@ class Board
 	  
   end
 
+  def woutput
+
+  	output = {
+	  "1" => %w[r r r r],
+	  "2" => %w[r r r w],
+	  "3" => %w[r r w w],
+	  "4" => %w[r w w w],
+	  "5" => %w[w w w w],
+	  "6" => %w[w w w],
+	  "7" => %w[w w],
+	  "8" => %w[w],
+	  "9" => %w[r r r],
+	  "10" => %w[r r],
+	  "11" => %w[r],
+	  "12" => %w[],
+	  "13" => %w[r r r r],
+	  "14" => %w[r r w],
+	  "15" => %w[r w],
+	  }
+
+	output.each do |key, value|
+	  if self == value
+	    print output.key(value)
+	  end
+	end
+
+  	self.all? { |element| element.to_s.empty? }
+  end
+
 end
 
